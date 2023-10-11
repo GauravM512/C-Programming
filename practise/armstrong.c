39b1aa4003c4623f03b3db99b3d02a61dbe1d44a
Gaurav Mandal <37218716+GauravM512@users.noreply.github.com>
2023-10-11 07:43:17 +0530

    Fix armstrong logic

--- practise/armstrong.c
+++ practise/armstrong.c
@@ -1,29 +1,37 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <math.h>
 
 int armstrong(int n)
 {
-    int sum=0,rem,temp=n;
-    while(n!=0)
+    int sum = 0, rem, temp = n;
+    int c = 0;
+    while (n != 0)
     {
-        rem=n%10;
-        sum=sum+rem*rem*rem;
-        n=n/10;
+        n = n / 10;
+        c++;
     }
-    if (sum==temp)
-        return 1;
+    n = temp;
+    while (n != 0)
+    {
+        rem = n % 10;
+        sum = sum + pow(rem, c);
+        n = n / 10;
+    }
+
+    if (sum == temp)
+        return 1; // It's an Armstrong number
     else
-        return sum;
+        return 0; // It's not an Armstrong number
 }
 
 int main()
 {
     int n;
     printf("Enter a number: ");
-    scanf("%d",&n);
-    if(armstrong(n))
-        printf("%d is an armstrong number\n",n);
+    scanf("%d", &n);
+    if (armstrong(n))
+        printf("%d is an Armstrong number\n", n);
     else
-        printf("%d is not an armstrong number\n",n);
+        printf("%d is not an Armstrong number\n", n);
     return 0;
-
 }
